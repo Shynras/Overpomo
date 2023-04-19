ac4d1c3273edca30c0c3de37861ae824d84a2c7e
Shynras <jacopofantuzzi@gmail.com>
2023-04-19 19:42:39 +0200

    app is now a PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,23 @@
+let CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+    }));
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response) {
+            console.log("Found response in cache:", response);
+            return response;
+        }
+        console.log("No response found in cache. About to fetch from network…");
+        return fetch(event.request);
+    }));
+});
