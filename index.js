f57ebb1f9b50fbb14603679738930708db97ca68
Shynras <jacopofantuzzi@gmail.com>
2023-04-25 02:30:30 +0200

    Added support for sass/scss

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import  { createRoot }  from 'react-dom/client';
-import App from './src/App.js'
+import './src/style.scss';
+import App from './src/App.js';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
