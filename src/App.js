c789bf21900815f5dde78b9f24420d6319c8d7b9
Shynras <jacopofantuzzi@gmail.com>
2023-05-18 16:34:41 +0200

    added bonus time feature logic

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import Timer from "./Timer.js";
 
 
 const App = () =>{
-    // app returns all the components needed to make the app
-    // you can have multiple components in multiple files, and import them here to add to app
-    // for now i'm just writing them here and if it gets too big i'll separate them
-    // not sure how it works when i have multiple pages, there'll be urls and for each url a different map? 
-    // or maybe in this file there'll be a conditional to provide different pages for different urls
-
-    //first component will be the button with the clock
-    // i don't think that i can use a button tag to create it, so i'm going to read a bit about button and span in react documentation
-
     return ( 
         <>
             <Timer className="clock"></Timer>
--- src/Timer.js
+++ src/Timer.js
@@ -1,41 +1,82 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 
 const Timer = (props) => {
-    const s = 5;
-    const m = 0;
-    const [active, setActive] = useState(false);
-    const [minutes, setMinutes] = useState(m);
+    let [s, m, bs, bm] = [5, 0, 0, 0];
+    const status = {paused: "PAUSED", running: "RUNNING", bonus: "BONUS"};
+    const [phase, setPhase] = useState(status.paused);
     const [seconds, setSeconds] = useState(s);
+    const [minutes, setMinutes] = useState(m);
+    const [bonusSeconds, setBonusSeconds] = useState(bs);
+    const [bonusMinutes, setBonusMinutes] = useState(bm);
     const interval = useRef();
 
-    const handleClick = () => {
-        setActive(!active);
-        const time = () => {
-            setSeconds((seconds) => {
-                if (seconds === 0) {
-                    if (minutes > 0) {
-                        setMinutes(minutes => minutes - 1);
-                        return 59;
-                    } else {
-                        console.log(interval.current)
-                        clearInterval(interval.current);
-                        setMinutes(m);
-                        return s;
-                    }
-                } else if (seconds === 1 && minutes === 0) {
-                    setActive(false);
-                    return 0;
-                } else {
-                    return seconds - 1;
-                }
-            });
-        }
-
-        if (!active) {
-            interval.current = setInterval(time, 1000);
+    const handleStyle = () => {
+        if (phase === status.running) {
+            return {backgroundColor: "green"};
+        } else if (phase === status.bonus) {
+            return {backgroundColor: "blue"};
         } else {
+            return {backgroundColor: "red"};
+        }
+    };
+
+    const handleClick = () => {
+        // State is asynchronous so I use "p" instead of "phase" inside setInterval
+        // I still need "phase" to re-render the timer with styles for different phases
+        let p;
+
+        const workTime = () => {
+            if (p === status.running) {
+                setSeconds(seconds => {
+                    if (seconds === 0) {
+                        if (minutes > 0) {
+                            setMinutes(minutes => minutes - 1);
+                            return 59;
+                        } else {
+                            p = status.bonus;
+                            setPhase(p);
+                            setBonusSeconds(1);
+                            return 0;
+                        }
+                    } else {
+                        return seconds - 1;
+                    }
+                });
+            } else if (p === status.bonus) {
+                setBonusSeconds(bonusSeconds => {    
+                    if (bonusSeconds === 59) {
+                        setBonusMinutes(bonusMinutes => bonusMinutes + 1);
+                        return 0;
+                    } else {
+                        return bonusSeconds + 1;
+                    }
+                });
+            }
+        };
+
+        if (phase === status.paused) {     
+            p = status.running; 
+            setPhase(p);
+            interval.current = setInterval(workTime, 1000);
+        } else {
+            p = status.paused;
+            setPhase(p);
+            setSeconds(s);
+            setMinutes(m);
+            setBonusSeconds(bs);
+            setBonusMinutes(bm); 
             clearInterval(interval.current);
+        }
+    };
+
+    const handleResult = () => {
+        if (phase !== status.bonus) {
+            return minutes.toLocaleString("en-us", {minimumIntegerDigits : 2}) + " : " +
+                seconds.toLocaleString("en-us", {minimumIntegerDigits: 2});
+        } else {
+            return bonusMinutes.toLocaleString("en-us", {minimumIntegerDigits : 2}) + " : " +
+                bonusSeconds.toLocaleString("en-us", {minimumIntegerDigits: 2});
         }
     };
 
@@ -43,10 +84,9 @@ const Timer = (props) => {
         <button 
             type="button" 
             className={props.className} 
-            style ={{backgroundColor: active ? "green" : "red"}} 
+            style={handleStyle()} 
             onClick={handleClick}>
-            {minutes.toLocaleString("en-us", {minimumIntegerDigits : 2}) + " : " +
-            seconds.toLocaleString("en-us", {minimumIntegerDigits: 2})}
+            {handleResult()}
         </button>
     );
 };
