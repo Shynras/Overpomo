bb91a500985563d64bcfd54bb5d12943c0b76298
Shynras <jacopofantuzzi@gmail.com>
2023-05-26 00:12:05 +0200

    uninstalled normalize.css because its already included in purecss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import  { createRoot }  from 'react-dom/client';
 import App from './App.js';
 import './style.scss';
 import 'purecss';
-import 'normalize.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
